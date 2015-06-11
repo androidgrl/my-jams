0465b8a2e62a390cf8dae1d271e1c461b95f6a67
androidgrl <kawaharajamie@gmail.com>
2015-06-10 20:59:06 -0600

    Add authorization functionality to view categories index for admin user

--- Gemfile
+++ Gemfile
@@ -43,5 +43,6 @@ group :development, :test do
   gem 'spring'
   gem 'capybara'
   gem 'launchy'
+  gem 'mocha'
 end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'it is valid' do
+    user = User.new(username: "Jamie",
+                    password: "password",
+                    role: 1)
+
+    assert user.valid?
+  end
+
+  test 'it is invalid without a username' do
+    user = User.new(username: "",
+                    password: "password",
+                    role: 0)
+
+    refute user.valid?
+  end
+
+  test 'username is unique' do
+    user1 = User.create(username: "Jamie",
+                    password: "password",
+                    role: 0)
+    user2 = User.new(username: "Jamie",
+                    password: "password",
+                    role: 0)
+
+    refute user2.valid?
+  end
 end
